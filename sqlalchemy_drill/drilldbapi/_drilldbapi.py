baaab5199912a47af09ace595af73735c66853b4
Nathan Baleeta <nbaleeta@gmail.com>
2024-05-02 16:39:49 -0700

    use original code - integrate yield function 2

--- sqlalchemy_drill/drilldbapi/_drilldbapi.py
+++ sqlalchemy_drill/drilldbapi/_drilldbapi.py
@@ -290,7 +290,7 @@ class Cursor(object):
                     logger.info(f'streamed {self.rownumber} rows.')
             
 
-            except StopIteration:
+            except:
                 #self.rowcount = self.rownumber
                 """ logger.info(
                     f'reached the end of the row data after {self.rownumber}'
