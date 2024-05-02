bb0b3306409fd6841ec6529c4b36c2c3ca80f208
Nathan Baleeta <nbaleeta@gmail.com>
2024-05-02 15:49:41 -0700

    use original code - integrate yield function modification with break

--- sqlalchemy_drill/drilldbapi/_drilldbapi.py
+++ sqlalchemy_drill/drilldbapi/_drilldbapi.py
@@ -269,36 +269,35 @@ class Cursor(object):
         fetch_until = self.rownumber + (size or self.arraysize)
         results = []
 
-        #generator_iterable = (x for x in self._row_stream)
+        while True:
+            try:
+                while self.rownumber != fetch_until:
+                    gen = self.mygenerator()
+                    row_dict = next(gen)
         
-        try:
-            while self.rownumber != fetch_until:
-                #row_dict = self.mygenerator(self._row_stream)
-                gen = self.mygenerator()
-                row_dict = next(gen)
-    
-                # values ordered according to self.result_md['columns']
-                row = [row_dict[col] for col in self.result_md['columns']]
+                    # values ordered according to self.result_md['columns']
+                    row = [row_dict[col] for col in self.result_md['columns']]
 
-                if self._typecaster_list is not None:
-                    row = (f(v) for f, v in zip(self._typecaster_list, row))
+                    if self._typecaster_list is not None:
+                        row = (f(v) for f, v in zip(self._typecaster_list, row))
 
-                results.append(tuple(row))
-                self.rownumber += 1
+                    results.append(tuple(row))
+                    self.rownumber += 1
 
-                if self.rownumber % api_globals._PROGRESS_LOG_N == 0:
-                    logger.info(f'streamed {self.rownumber} rows.')
-        
+                    if self.rownumber % api_globals._PROGRESS_LOG_N == 0:
+                        logger.info(f'streamed {self.rownumber} rows.')
+            
 
-        except StopIteration:
-            self.rowcount = self.rownumber
-            logger.info(
-                f'reached the end of the row data after {self.rownumber}'
-                ' records.'
-            ) 
+            except StopIteration:
+                self.rowcount = self.rownumber
+                logger.info(
+                    f'reached the end of the row data after {self.rownumber}'
+                    ' records.'
+                ) 
 
-            # restart the outer parsing loop to collect trailing metadata
-            self._outer_parsing_loop()     
+                # restart the outer parsing loop to collect trailing metadata
+                self._outer_parsing_loop() 
+                break    
                     
         return results
             
