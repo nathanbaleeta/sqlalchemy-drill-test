a4765d01350032c00823fa8a5e5a2c91224c48f5
Nathan Baleeta <nbaleeta@gmail.com>
2024-05-02 15:39:48 -0700

    use original code - integrate yield function modify

--- sqlalchemy_drill/drilldbapi/_drilldbapi.py
+++ sqlalchemy_drill/drilldbapi/_drilldbapi.py
@@ -273,8 +273,12 @@ class Cursor(object):
         
         try:
             while self.rownumber != fetch_until:
-                row_dict = self.mygenerator(self._row_stream)
+                #row_dict = self.mygenerator(self._row_stream)
 
+                gen = self.mygenerator(self._row_stream)
+
+                row_dict = [element for element in gen]
+    
                 # values ordered according to self.result_md['columns']
                 row = [row_dict[col] for col in self.result_md['columns']]
 
