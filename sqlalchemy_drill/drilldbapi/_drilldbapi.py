c704dddeca7ebcb88726a11afb20889c47d4c748
Nathan Baleeta <nbaleeta@gmail.com>
2024-05-02 13:03:01 -0700

    use break to stop iteration - comment out self._outer_parsing_loop()

--- sqlalchemy_drill/drilldbapi/_drilldbapi.py
+++ sqlalchemy_drill/drilldbapi/_drilldbapi.py
@@ -301,7 +301,7 @@ class Cursor(object):
                     )
 
                 # restart the outer parsing loop to collect trailing metadata
-                self._outer_parsing_loop()     
+                #self._outer_parsing_loop()     
                 break  
             
         
